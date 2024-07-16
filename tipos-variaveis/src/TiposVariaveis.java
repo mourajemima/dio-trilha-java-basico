139440214089be7e36a27d6cc52384575aaf67ed
Jemima Moura <jemimahelen@hotmail.com>
2024-07-16 11:54:02 -0300

    tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //tipos primitivos
+        int numero = 5;
+        numero = 6;
+
+        final double PI = 3.14;
+
+        System.out.println(numero);
+        System.out.println(PI);
+        
+        //string
+        String nome = "Jemima";
+
+        System.out.print(nome);
+
     }
 }
